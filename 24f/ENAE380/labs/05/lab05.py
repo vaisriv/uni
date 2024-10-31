8e9b176dc40550130beaafbe5069a119722dec26
Vai <vai.sriv@icloud.com>
2024-10-30 20:14:48 -0400

    start enae380 lab05

--- 24f/ENAE380/labs/05/lab05.py
+++ 24f/ENAE380/labs/05/lab05.py
@@ -5,10 +5,13 @@ function. You are encouraged to implement helper functions as needed with
 a short (one-line) description of their purpose.
 """
 
-import cv2
+import cv2 as cv
 import time
+from cv2.gapi import threshold
+import numpy as np
 
-class Lab5():
+
+class Lab5:
 
     def calculate_spectrum(self, filePath):
         """
@@ -23,9 +26,12 @@ class Lab5():
         -------
         result : (int, int, int)
             total intesities in each of the BGR color channels respectively
-
         """
-
+        img = cv.imread(filePath)
+        sum_blue = int(np.sum(img[:, :, 0]))
+        sum_green = int(np.sum(img[:, :, 1]))
+        sum_red = int(np.sum(img[:, :, 2]))
+        return (sum_blue, sum_green, sum_red)
 
     def get_dominant_color(self, filePath):
         """
@@ -40,9 +46,8 @@ class Lab5():
         -------
         int
             0 for Blue, 1 for Green, 2 for Red
-
         """
-
+        return np.argmax(self.calculate_spectrum(filePath))
 
     def measure_runtime_numpy(self):
         """
@@ -53,7 +58,10 @@ class Lab5():
         float
             time taken to run timing_numpy
         """
-
+        starttime = time.time()
+        self.timing_numpy("./test_images/test_image.png")
+        endtime = time.time()
+        return endtime - starttime
 
     def measure_runtime_python(self):
         """
@@ -63,34 +71,33 @@ class Lab5():
         -------
         float
             time taken to run timing_python
-
         """
-
+        starttime = time.time()
+        self.timing_python("./test_images/test_image.png")
+        endtime = time.time()
+        return endtime - starttime
 
     def timing_numpy(self, filePath):
-        """ Accesses each pixel of an image via numpy. Do not modify this function. """
-        IMG=cv2.imread(filePath)
-        info=IMG.shape
-        for row in range(0,info[0]):
-            for col in range(0,info[1]):
-                Blue_pixel=IMG.item(row,col,0)
-                Green_pixel=IMG.item(row,col,1)
-                Red_pixel=IMG.item(row,col,2)
-
+        """Accesses each pixel of an image via numpy. Do not modify this function."""
+        IMG = cv.imread(filePath)
+        info = IMG.shape
+        for row in range(0, info[0]):
+            for col in range(0, info[1]):
+                Blue_pixel = IMG.item(row, col, 0)
+                Green_pixel = IMG.item(row, col, 1)
+                Red_pixel = IMG.item(row, col, 2)
 
     def timing_python(self, filePath):
-        """ Accesses each pixel of an image via python lists. Do not modify this function. """
-        IMG=cv2.imread(filePath)
-        info=IMG.shape
-        for row in range(0,info[0]):
-            for col in range(0,info[1]):
-                Blue_pixel=IMG[row][col][0]
-                Green_pixel=IMG[row][col][1]
-                Red_pixel=IMG[row][col][2]
+        """Accesses each pixel of an image via python lists. Do not modify this function."""
+        IMG = cv.imread(filePath)
+        info = IMG.shape
+        for row in range(0, info[0]):
+            for col in range(0, info[1]):
+                Blue_pixel = IMG[row][col][0]
+                Green_pixel = IMG[row][col][1]
+                Red_pixel = IMG[row][col][2]
 
-
-
-    def blur_image(self, filePath):
+    def blur_image(self, infilePath, outfilePath):
         """
         Blurs the input image using a Guassian filter.
         Note that the function takes a file path, not a loaded image, as input,
@@ -105,12 +112,13 @@ class Lab5():
         -------
         numpy.ndarray
             the newly generated image
-
         """
+        inimg = cv.imread(infilePath)
+        outimg = cv.GaussianBlur(inimg, (5, 5), 0)
+        cv.imwrite(outfilePath, outimg)
+        return outimg
 
-
-
-    def findoutlines(self, filePath):
+    def findoutlines(self, infilePath, outfilePath):
         """
         Outputs and writes an image outlining the shapes contained in the input image
 
@@ -124,10 +132,29 @@ class Lab5():
         numpy.ndarray
             the newly generated image
         """
+        inimg = cv.imread(infilePath)
 
-'''
-if __name__ == '__main__':
-    l = Lab5()
-    b = l.findoutlines('files/BlueSquare.png')
-    print(b)
-'''
+        gray = cv.cvtColor(inimg, cv.COLOR_BGR2GRAY)
+        _, thresh = cv.threshold(gray, 1, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+        contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+        outimg = inimg
+        cv.drawContours(outimg, contours, -1, (255, 255, 255), 2)
+        cv.imwrite(outfilePath, outimg)
+        return outimg
+
+
+if __name__ == "__main__":
+    lab = Lab5()
+    print("test_img spectrum:", lab.calculate_spectrum("./test_images/test_image.png"))
+    print("test_img max: index", lab.get_dominant_color("./test_images/test_image.png"))
+    print("numpy:", lab.measure_runtime_numpy(), "seconds")
+    print("python:", lab.measure_runtime_numpy(), "seconds")
+    print(
+        "blur test:",
+        lab.blur_image("./test_images/BlurMe.png", "./test_images/Srivastava_2a.png"),
+    )
+    print(
+        "outline test:",
+        lab.findoutlines("./test_images/BlueSquare.png", "./test_images/Srivastava_2b.png"),
+    )
--- 24f/ENAE380/labs/05/stereo.py
+++ 24f/ENAE380/labs/05/stereo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage, skimage.io 
+import skimage
+import skimage.io
 
 
 
