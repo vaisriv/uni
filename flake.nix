9cb341fe6a1d4f28390948aea3502e801c944ca8
vai <vai.sriv@icloud.com>
2025-02-03 22:58:48 -0500

    create basic python nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+	description = "dev-shell flake";
+
+	inputs = {
+		# Nix Packages
+		nixpkgs.url = "nixpkgs/nixos-24.11";
+
+		# Better Nix Implementation
+		lix-module = {
+			url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		# Generic Systems for Flakes
+		systems = {
+			url = "github:nix-systems/default";
+		};
+
+		# Flake Utils
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+			inputs.systems.follows = "systems";
+		};
+	};
+
+	outputs = {
+		nixpkgs,
+		flake-utils,
+		...
+	}:
+		flake-utils.lib.eachDefaultSystem (
+			system: let
+				pkgs = nixpkgs.legacyPackages.${system};
+			in {
+				formatter = pkgs.alejandra;
+				devShells.default =
+					pkgs.mkShell {
+						packages = with pkgs; [
+							# python
+							(python3.withPackages (ps:
+										with ps; [
+											matplotlib
+											numpy
+										]))
+							basedpyright
+							ruff
+						];
+					};
+			}
+		);
+}
