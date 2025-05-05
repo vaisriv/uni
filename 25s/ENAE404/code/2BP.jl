8eba551ca29cde56e6645859554fdbbb8b4a2ff3
vai <vai.sriv@icloud.com>
2025-05-05 18:53:29 -0400

    work on enae404 hw07

--- 25s/ENAE404/code/2BP.jl
+++ 25s/ENAE404/code/2BP.jl
@@ -9,23 +9,22 @@ function two_body!(du, u, μ, t)
 end
 
 function two_body_thrust!(du, u, params, t)
-    μ, T_spec_kN_per_kg = params
+	μ, T_spec_kN_per_kg = params
 
-    # unpack position & velocity views
-    @views du[1:3] .= u[4:6]
-    r = @view u[1:3]
-    v = @view u[4:6]
+	# unpack position & velocity views
+	@views du[1:3] .= u[4:6]
+	r = @view u[1:3]
+	v = @view u[4:6]
 
-    # gravity
-    r_norm = norm(r)
-    grav_acc = -μ .* r ./ (r_norm^3)
+	# gravity
+	r_norm = norm(r)
+	grav_acc = -μ .* r ./ (r_norm^3)
 
-    # thrust: convert kN/kg to m/s^2 by *1000 (1 kN/kg = 1000 m/s^2)
-    a_thrust_mag = T_spec_kN_per_kg * 1e3
-    v_norm = norm(v)
-    thrust_acc = v_norm > 0 ? a_thrust_mag .* (v ./ v_norm) : zero(v)
+	a_thrust_mag = T_spec_kN_per_kg
+	v_norm = norm(v)
+	thrust_acc = v_norm > 0 ? a_thrust_mag .* (v ./ v_norm) : zero(v)
 
-    @views du[4:6] .= grav_acc .+ thrust_acc
+	@views du[4:6] .= grav_acc .+ thrust_acc
 end
 
 function solve_2BP(initial::StateVectors,
@@ -47,25 +46,25 @@ function solve_2BP(initial::StateVectors,
 end
 
 function solve_2BP_thrust(initial::StateVectors,
-                          tspan::Tuple{Float64, Float64};
-                          μ::Float64 = μ_Earth,
-                          T_spec::Float64 = 1e-4,
-                          reltol::Float64 = 1e-9,
-                          abstol::Float64 = 1e-9,
-                          int_pts::Int64 = 2)
+			  tspan::Tuple{Float64, Float64};
+			  μ::Float64 = μ_Earth,
+			  T_spec::Float64 = 1e-4,
+			  reltol::Float64 = 1e-9,
+			  abstol::Float64 = 1e-9,
+			  int_pts::Int64 = 2)
 
-    # pack initial state
-    u0 = vcat(initial.r, initial.v)
+	# pack initial state
+	u0 = vcat(initial.r, initial.v)
 
-    # setup and solve ODE problem
-    prob = ODEProblem(two_body_thrust!, u0, tspan, (μ, T_spec))
-    sol  = solve(prob, Tsit5();
-                 reltol = reltol,
-                 abstol = abstol,
-                 saveat = range(tspan[1], tspan[2], length=int_pts))
+	# setup and solve ODE problem
+	prob = ODEProblem(two_body_thrust!, u0, tspan, (μ, T_spec))
+	sol  = solve(prob, Tsit5();
+	      reltol = reltol,
+	      abstol = abstol,
+	      saveat = range(tspan[1], tspan[2], length=int_pts))
 
-    # unpack back into StateVectors
-    return [ StateVectors(u[1:3], u[4:6]) for u in sol.u ]
+	# unpack back into StateVectors
+	return [ StateVectors(u[1:3], u[4:6]) for u in sol.u ]
 end
 
-export solve_2BP solve_2BP_thrust
+export solve_2BP, solve_2BP_thrust
--- 25s/ENAE404/hw/hw07/code/s01.jl
+++ 25s/ENAE404/hw/hw07/code/s01.jl
@@ -1,11 +1,58 @@
+# enae404 hw07
 include("../../../code/sfd.jl")
 using .SpaceFlightDynamics
+using LinearAlgebra
+using Plots
 
-r1 = [8000.0, 0.0, 0.0]
+# problem 01
+# givens
+Tₛ = 1e-4
+rₒ = 8000.0
+r₁ = rₒ * [1.0, 0.0, 0.0]
 
-v1 = []
-t_esc = 3600.0
+# part a
+vₒ = sqrt(μ_Earth/rₒ)
+v₁ = vₒ * [0.0, 1.0, 0.0]
+@show v₁
 
-sv = solve_2BP(StateVectors(r1, v1), (0.0, t_esc), μ=μ_Earth, T_spec=1e-4, int_pts = 500)
+# part b
+aᵣ = -1*μ_Earth/rₒ^2
+aₜ = Tₛ
+a = [aᵣ, aₜ]
+@show a
 
-println("  sv = ", sv)
+# part c
+tₑ = vₒ/aₜ*(1-(20*aₜ^2*rₒ^2/vₒ^9)^(1/8))
+@show tₑ
+
+# part d
+sv = solve_2BP_thrust(StateVectors(r₁, v₁), (0.0, 2*tₑ), μ=μ_Earth, T_spec=Tₛ, int_pts=500)
+vₑ = sv[end].v
+@show vₑ
+
+xs = [sv.r[1] for sv in sv]
+ys = [sv.r[2] for sv in sv]
+plt = plot(
+	xs, ys, label = "2BP Integration",
+	title = "Thrust Escape Trajectory",
+	xlabel = "x (km)",
+	ylabel = "y (km)",
+	aspect_ratio = :equal,
+	grid = true)
+display(plt)
+
+# part e
+time_step = 2*tₑ/length(sv)
+rₑ = rₒ*vₒ/((20*aₜ^2*rₒ^2)^(1/4))
+@show time_step
+@show rₑ
+tₑ_num = 0
+for i ∈ 1:length(sv)
+	if norm(sv[i].r) >= rₑ
+		@show norm(sv[i].r)
+		@show i
+		global tₑ_num = i*time_step
+		break
+	end
+end
+@show tₑ_num
